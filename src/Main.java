21de6f01d84592f8b5d5ea34c08549400b1eb5e3
still-not-telling <JR212384@reigate.ac.uk>
2021-11-30 10:12:42 +0000

    project now finished

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,67 @@
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static String[] guessBoard = {"", "", "", "", ""};
-
+    public static Scanner input = new Scanner(System.in);
 
     public static void julieTurn() {
+        Random Random = new Random();
+        int julieGuess = Random.nextInt(5);
+        if (guessBoard[julieGuess].isEmpty()) {
+            guessBoard[julieGuess] = ("julie");
+        }
+    }
 
+    public static boolean userGuessValidCheck(int userGuess) {
+        if (userGuess > 0 && userGuess < 5) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static int userGuessTypeCheck() {
+        int userGuess;
+        while (true) {
+            try {
+                userGuess = input.nextInt();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("your input was not a number , please try again");
+                input.next();
+            }
+        }
+        return userGuess;
     }
 
     public static int getUserGuess() {
-        System.out.println("its your turn , enter a number between 0-4");
-        Scanner input = new Scanner(System.in);
-        return input.nextInt();
+        int userGuess = 0;
+
+        boolean userGuessValid = false;
+        while (!userGuessValid) {
+            System.out.println("please enter a number between 0-4");
+            userGuess = userGuessTypeCheck();
+            userGuessValid = userGuessValidCheck(userGuess);
+        }
+        return userGuess;
     }
 
     public static String getUserName() {
         System.out.println("please enter your name");
-        Scanner input = new Scanner(System.in);
         return input.next();
     }
 
     public static void playerTurn(int playerGuess, String playerName) {
-
+        if (guessBoard[playerGuess].isEmpty()) {
+            guessBoard[playerGuess] = (playerName);
+        }
     }
 
     public static void main(String[] args) {
+        int julieScore = 0;
+        int playerScore = 0;
         System.out.println("welcome to the guessing game, please enter your name to start");
         boolean win = false;
         String playerName = getUserName();
@@ -34,8 +70,27 @@ public class Main {
 
             int playerGuess = getUserGuess();
             playerTurn(playerGuess, playerName);
-
-
+            System.out.println("input successful , computer has taken its turn");
+            julieTurn();
+            for (int i = 0; i < guessBoard.length; i++) {
+                if (guessBoard[i].isEmpty()) {
+                    win = false;
+                    break;
+                } else {
+                    win = true;
+                }
+            }
         }
+        for (int j = 0; j < guessBoard.length; j++) {
+            if (guessBoard[j].contains("julie")) {
+                julieScore++;
+            } else {
+                playerScore++;
+            }
+        }
+        if(playerScore>julieScore){
+            System.out.println("you scored "+playerScore+" the computer scored "+julieScore+ " you win!");
+        }else{
+            System.out.println("you scored "+playerScore+" the computer scored "+julieScore+ " you lose!");
     }
-}
+}}
